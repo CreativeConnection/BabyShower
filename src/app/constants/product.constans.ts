76eadd3455dffaf5479977b07278dbb8e9edf170
PaulaMendoza01 <ingpaulamendoza@gmail.com>
2022-06-21 17:48:59 -0500

    :art: Actualizando productos

--- src/app/constants/product.constans.ts
+++ src/app/constants/product.constans.ts
@@ -2,21 +2,100 @@ import { IProduct } from '../models/product.interface';
 
 export const products: IProduct[] = [
   {
-    title: 'Cambiador bebe',
-    image: 'https://alapalma.com/wp-content/uploads/412Y-PxjUlL._SS500_.jpg',
-    buyer: 'Davixon',
+    title: 'Toalla de baño',
+    image: '../../../assets/images/coche.jpg',  
     purchased: false,
   },
   {
-    title: 'Cambiador bebe',
-    image: 'https://alapalma.com/wp-content/uploads/412Y-PxjUlL._SS500_.jpg',
-    buyer: 'Davixon',
+    title: 'Cobija',
+    image: '../../../assets/images/coche.jpg',  
     purchased: false,
   },
   {
-    title: 'Cambiador bebe',
-    image: 'https://alapalma.com/wp-content/uploads/412Y-PxjUlL._SS500_.jpg',
-    buyer: 'Davixon',
+    title: 'colchón corral 1m * 0.7 m',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Sábanas corral 1m * 0.7 m',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Teteros Avent',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Gorros',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Baberos',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Ropa talla 6 meses en adelante',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'kit de cuidado',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'chupo de fruta',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Soporte de baño para la tina',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'juguetes',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'Lamparita',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'móvil para cuna',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'almohada media Luna',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'pañales de tela',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'cambiador portátil',
+    image: '../../../assets/images/coche.jpg',  
+    buyer: 'Davinxon',
+    purchased: false,
+  },
+  {
+    title: 'cojin nido bebé',
+    image: '../../../assets/images/coche.jpg',  
+    purchased: false,
+  },
+  {
+    title: 'gimnasio de actividades',
+    image: '../../../assets/images/coche.jpg',  
+    buyer: 'Camilo Veloza',
     purchased: false,
   },
   {
@@ -27,182 +106,28 @@ export const products: IProduct[] = [
     purchased: true,
   },
   {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
+    title: 'Coche',
+    image: '../../../assets/images/coche.jpg',  
+    buyer: 'Nonito Angel',
     purchased: true,
   },
   {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
+    title: 'Tapete tejido',
+    image: '../../../assets/images/coche.jpg',  
+    buyer: 'Nonito Nidya',
     purchased: true,
   },
   {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
+    title: 'Tina',
+    image: '../../../assets/images/tina.jpg',  
+    buyer: 'Papitos',
     purchased: true,
   },
   {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
+    title: 'Silla moms',
+    image: '../../../assets/images/sillaMoms.jpg',
+    buyer: 'Papitos',
     purchased: true,
   },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Corral',
-    image:
-      'https://cdn.shopify.com/s/files/1/1886/9469/products/Untitleddesign-2021-07-03T160358.265.png?v=1630935982',
-    buyer: 'Nonita Belsy',
-    purchased: true,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
-  {
-    title: 'Jefe',
-    image:
-      'https://i2.wp.com/www.senpai.com.mx/wp-content/uploads/2021/11/Halo-Quien-es-el-Jefe-Maestro-compressed.jpg?w=1280&ssl=1',
-    purchased: false,
-  },
+
 ];
